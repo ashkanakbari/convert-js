348a01431f190c086f816647405c89dbe5a3d463
ashkan <49208733+ashkanakbari@users.noreply.github.com>
2019-05-13 11:31:18 -0700

    Add files via upload

---
+++ exercice/js/main.js
@@ -0,0 +1,14 @@
+alert("Convert");
+
+
+function lenghtConverter(valNum) {
+  document.getElementById("outputcm").innerHTML=valNum/0.39370079;
+}
+
+
+
+
+function inchConverter(valNum) {
+  document.getElementById("outputinch").innerHTML=valNum/2.54;
+
+}
